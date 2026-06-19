e738978159751b83267211fdb7b077b4509b6640
Nikhil <nikhilkumaraf309@gmail.com>
2026-06-19 09:32:58 +0530

    Another problem solved in contest

---
+++ contest_question/codeforces/A -Destroying Tower.cpp
@@ -0,0 +1,65 @@
+/*
+Name = Nikihl
+github =https://github.com/N1kh1lKumar
+linked in = https://www.linkedin.com/in/n1kh1lkumar/
+leetcode = https://leetcode.com/u/N1kh1lKumar/
+codolio =  https://codolio.com/profile/N1kh1lKumar
+codeforces = https://codeforces.com/profile/nikhilkumaraf309
+Unstop = https://unstop.com/u/nikhilps68134
+
+Contact Email = nikhilkumaraf309@gmail.com 
+*/
+
+// some useful alias 
+using ll = long long;
+using dl =  long double;
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n ; 
+    cin >> n;
+
+    vector<int> v(n);
+    for ( int i =0; i<n; i++){
+        cin >> v[i];
+    }
+
+
+
+    for ( int i=0; i<n-1; i++){
+        if(v[i] > v[i+1]){
+            continue;
+        }
+        else{
+            v[i+1] = v[i];
+        }
+    }
+
+    int sum = 0;
+    for ( int i =0; i<n; i++){
+        sum += v[i];
+    }
+
+
+    cout <<  sum << "\n";
+}
+
+int main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; 
+    cin >> t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
